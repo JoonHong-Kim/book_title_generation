bb620168bb74503ce5715e8544a96fdc7417c8d5
JoonHong-Kim <danielkim30433@gmail.com>
2021-11-19 09:21:01 +0000

    fea) Implement dataset class

--- code/data/dataset.py
+++ code/data/dataset.py
@@ -8,9 +8,10 @@ class Dataset(data.Dataset):
     def __init__(self,description:str,title:str):
         self.description=description
         self.title=title
-    
+
     def __getitem__(self,index:int)->Tuple[str,str]:
-        return (self.description[index],self.title[index])
+        item={description: self.description[index].clone().detach(),title: self.title[index].clone().detach()}
+        return item
     
     def __len__(self)->int:
         return len(self.title)
