26ec188146de8f79cebae581869705bb05c56a4d
JoonHong-Kim <danielkim30433@gmail.com>
2021-11-21 11:52:40 +0000

    fea) correct the error

--- code/data/dataset.py
+++ code/data/dataset.py
@@ -2,45 +2,34 @@ from torch.utils.data import Dataset
 import numpy as np
 
 # 참고: https://github.com/seujung/KoBART-translation/blob/main/dataset.py
-class Dataset(Dataset):
+class KoBARTDataset(Dataset):
     """
     data를 model에 넣기위해 사용되는 dataset class
     """
 
     def __init__(self, dataset, tokenizer):
-        super.__init__()
+        super().__init__()
         self.dataset = dataset
         self.tokenizer = tokenizer
 
     def add_padding_data(self, inputs):
         if len(inputs) < 512:
-            pad = np.array([self.pad_index] * (512 - len(inputs)))
+            pad = np.array([self.tokenizer.pad_token_id] * (512 - len(inputs)))
             inputs = np.concatenate([inputs, pad])
         else:
             inputs = inputs[:512]
 
-        return inputs
-
-    def add_ignored_data(self, inputs):
-        if len(inputs) < 512:
-            pad = np.array([self.ignore_index] * (512 - len(inputs)))
-            inputs = np.concatenate([inputs, pad])
-        else:
-            inputs = inputs[:512]
         return inputs
 
     def __getitem__(self, index):
         item = self.dataset.iloc[index]
-        input_ids = self.tokenizer.encode(item["descriptions"], add_special_tokens=True)
+        input_ids = self.tokenizer.encode(item["description"], add_special_tokens=True)
         input_ids = self.add_padding_data(input_ids)
 
         labels = self.tokenizer.encode(item["title"], add_special_tokens=True)
-
+        labels = self.add_padding_data(labels)
         decoder_input_ids = [self.tokenizer.pad_token_id]
         decoder_input_ids += labels[:-1]
-        decoder_input_ids = self.add_padding_data(decoder_input_ids)
-
-        labels = self.add_ignored_data(labels)
 
         return {
             "input_ids": np.array(input_ids, dtype=np.int_),
